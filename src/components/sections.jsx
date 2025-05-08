7fff6e3be1f1cc073c36dd2db171536e4c5a5b9f
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-08 23:43:47 +0400

    sections component

---
+++ src/components/sections.jsx
@@ -0,0 +1,27 @@
+const Main = () =>{
+    return (
+        <div>
+            <div className="Main">
+                <div className="Container2">
+                    <h1 className="MainTitle">
+                    СУПЕР ТРОФЕО МИРОВОЙ ФИНАЛ
+                    </h1>
+                    <div className="MainDescBox">
+                        <div className="MainDescTitleBox">
+                            <p className="MainDescTitle">
+                            Мировой финал Lamborghini завершает каждый сезон Super Trofeo с 2013 года. Каждый год гонщики всех континентальных чемпионатов отправляются на разные трассы и принимают участие в двух 50-минутных спринтерских гонках, где определяется победитель в общем зачете. как чемпион мира Lamborghini. Мировой финал Lamborghini — это не только крупнейший гоночный уик-энд года, но и настоящий фестиваль автоспорта, который привлекает поклонников со всего мира.
+                            </p>
+                            <p className="MainDescTitle2">
+                            Парад владельцев, превью и развлечения в паддоке для болельщиков — это лишь некоторые из мероприятий, организованных в рамках мероприятия.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="MainDescImgBox">
+                        <img src="./public/MainSectionSvg.svg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Main
