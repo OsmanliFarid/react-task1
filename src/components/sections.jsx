2f5a7e43b8b2586d04f5003b3d7478231759bf41
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-09 13:04:24 +0400

    sections3 end

--- src/components/sections.jsx
+++ src/components/sections.jsx
@@ -6,7 +6,7 @@ const Main = () =>{
                     <h1 className="MainTitle">
                     СУПЕР ТРОФЕО МИРОВОЙ ФИНАЛ
                     </h1>
-                    <div className="MainDescBox">
+                    <div className="MainDescBoxs">
                         <div className="MainDescTitleBox">
                             <p className="MainDescTitle">
                             Мировой финал Lamborghini завершает каждый сезон Super Trofeo с 2013 года. Каждый год гонщики всех континентальных чемпионатов отправляются на разные трассы и принимают участие в двух 50-минутных спринтерских гонках, где определяется победитель в общем зачете. как чемпион мира Lamborghini. Мировой финал Lamborghini — это не только крупнейший гоночный уик-энд года, но и настоящий фестиваль автоспорта, который привлекает поклонников со всего мира.
@@ -18,6 +18,65 @@ const Main = () =>{
                     </div>
                     <div className="MainDescImgBox">
                         <img src="./public/MainSectionSvg.svg" alt="" />
+                    </div>
+                    <div className="MainDescBoxs2">
+                        <div className="MainDescTitleBox2">
+                            <h1 className="MainDescHeaderTitle">
+                            КОМАНДЫ СУПЕР ТРОФЕО
+                        </h1>
+                        <p className="MainDescTitle3">
+                            Все автомобили, которые участвуют в чемпионатах Lamborghini Super Trofeo, управляются частными командами, которые используют свой опыт для тонкой настройки их для водителей. Для каждого континентального чемпионата Lamborghini Super Trofeo существуют рейтинги пилотов и команд.
+                        </p>
+                        <p className="MainDescTitle4">
+                            Пилоты и команды, участвующие в континентальной серии, каждый год соревнуются друг с другом в Гранд Финале Lamborghini, мероприятии, которое завершает спортивный сезон Super Trofeo. Две квалификационные сессии и две гонки определяют чемпионов мира серии. Каждый год Гранд-финал проводится на разных трассах, и в прошлых выпусках он уже затрагивал европейский, азиатский и американский континенты.
+                        </p>
+                        </div>
+                        <div className="MainDescImgBox2">
+                            <img src="./public/MainDescBack.png" alt="" />
+                        </div>
+ 
+                    </div>
+                    <div className="Section3Boxs">
+                        <div className="Section3BoxTitle">
+                            <h1 className="Section3Title">
+                                календарь сезона Super Trofeo Europe 2022 года
+                            </h1>
+                            <p className="Section3DescTitle">
+                                14 в истории серии GT, посвященный автомобилям Lamborghini Huracan Super Trofeo Evo. Шесть этапов, в общей сложности 12 гонок и 600 минут соревнований будут разыграны на некоторых из самых престижных трасс Европы.
+                            </p>
+                        </div>
+                        <div className="Section3Table">
+                            <ul className="Section3Cart">
+                                <li>Этап 1: </li>
+                                <li>Этап 2: </li>
+                                <li>Этап 3: </li>
+                                <li>Этап 4: </li>
+                                <li>Этап 5: </li>
+                                <li>Этап 6: </li>
+                                <li>Гранд-финал 2022 : </li>
+                            </ul>
+                                <ul className="Section3Cart2">
+                                    <li>
+                                        Имола (Италия)
+                                    </li>
+                                    <li>Ле-Кастелле (Франция)</li>
+                                    <li>Мизано (Италия)</li>
+                                    <li>Мизано (Италия)</li>
+                                    <li>Мизано (Италия)</li>
+                                    <li>Мизано (Италия)</li>
+                                    <li>Мизано (Италия)</li>
+                                </ul>
+                                <ul className="Section3Cart3">
+                                   <li>1-3 апреля</li>
+                                   <li>4-3 апреля</li>
+                                   <li>1-3 апреля</li>
+                                   <li>2-3 апреля</li>
+                                   <li>1-3 апреля</li>
+                                   <li>6-3 апреля</li>
+                                   <li>1-3 апреля</li>
+                                </ul>
+                            
+                        </div>
                     </div>
                 </div>
             </div>
